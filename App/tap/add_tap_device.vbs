52c036c1880f6ac6fd826c78b100e96f313934ac
lzdnico <596049368@qq.com>
2020-07-31 20:21:20 +0800

    remove usless command

--- App/tap/add_tap_device.vbs
+++
@@ -1,1 +0,0 @@
-CreateObject("WScript.Shell").Run "add_tap_device.bat",0
--- App/tap/ahktapstart.bat
+++ App/tap/ahktapstart.bat
@@ -4,8 +4,5 @@ set DEVICE_NAME=clash-tap
 set PATH=%PATH%;"%~dp0App";"%~dp0App\tap";%SystemRoot%\system32;%SystemRoot%\system32\wbem;%SystemRoot%\system32\WindowsPowerShell/v1.0
 cd ./App
 cd ./tap
-add_tap_device.vbs
 tun2socks.vbs
-netsh interface ip add route 0.0.0.0/0 %DEVICE_NAME% 10.0.0.1 metric=0 store=active
-netsh interface ipv6 add route ::/0 %DEVICE_NAME% fdfe:dcba:9876::1 metric=0 store=active
 ipconfig /flushdns
